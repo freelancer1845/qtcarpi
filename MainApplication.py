9b48e29d97946e5cf0c3fb5ae2b415f07372d139
freelancer1845 <jaschelite@googlemail.com>
2017-07-19 00:47:49 +0200

    Removed CharsetSetting in Skin.css added qMessageHandler

--- MainApplication.py
+++ MainApplication.py
@@ -3,10 +3,16 @@ Created on 10.06.2017
 
 @author: Jascha Riedel
 '''
+from PyQt5 import QtCore
 from PyQt5.Qt import QApplication, QFontDatabase
 from Gui.MainWindow import MainWindow
 from Backend.core.MainController import MainController
 from PyQt5.QtCore import qCritical
+from PyQt5.QtCore import qDebug
+from PyQt5.QtCore import QtInfoMsg
+from PyQt5.QtCore import QtWarningMsg
+from PyQt5.QtCore import QtCriticalMsg
+from PyQt5.QtCore import QtFatalMsg
 import logging
 
 
@@ -17,12 +23,12 @@ class MainApplication(QApplication):
 
 
     def loadSkin(self):
-        
+        qDebug('Loading skin...')
         try:
             with open('resources/skin.css', 'r') as handle:
                 skinCss = handle.read()
                 self.setStyleSheet(skinCss)
-        except IOError:
+        except IOError as err:
             qCritical('Failed to load skin file.')
     
     
@@ -31,6 +37,9 @@ class MainApplication(QApplication):
         Constructor
         '''
         super().__init__(argv)
+        
+        #initalize sysout logging
+        QtCore.qInstallMessageHandler(MainApplication.qt_message_handler)
         
         #set log level
         logging.basicConfig(level= logging.DEBUG)
@@ -43,4 +52,21 @@ class MainApplication(QApplication):
         
         # Create the Main Window
         self.mainWindow = MainWindow()
+        
+    @classmethod
+    def qt_message_handler(cls, mode, context, message):
+        if mode == QtInfoMsg:
+            mode = 'INFO'
+        elif mode == QtWarningMsg:
+            mode = 'WARNING'
+        elif mode == QtCriticalMsg:
+            mode = 'CRITICAL'
+        elif mode == QtFatalMsg:
+            mode = 'FATAL'
+        else:
+            mode = 'DEBUG'
+        print('qt_message_handler: line: %d, func: %s(), file: %s' % (
+            context.line, context.function, context.file))
+        print('  %s: %s\n' % (mode, message))
+        
         
